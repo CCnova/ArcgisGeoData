0e9a8b5b66dc35902168335a2b94cfbbaf186d60
CCnova <caiocnova1@gmail.com>
2021-01-11 17:05:49 -0300

    Adiciona funcionalidade de esconder e mostrar layers, alem de popup ao clicar em um ponto do mapa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    NoopAnimationsModule,
+    MatCardModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   declarations: [
     AppComponent,
     MapComponent,
-  ],
-  imports: [
-    BrowserModule
   ],
   providers: [],
   bootstrap: [
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, Output, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { loadModules } from 'esri-loader';
 
 @Component({
@@ -9,31 +10,112 @@ import { loadModules } from 'esri-loader';
 
 export class MapComponent implements OnInit {
   @ViewChild('mapViewElement') private mapViewElement: ElementRef;
+  @Output() public layersFormGroup: FormGroup;
+  public layers: any[];
+  public populacaoLayer: any;
+  public idhmLayer: any;
+  public pibLayer: any;
+  public arcgisPortal: any;
+  public portalMap: any;
+  public mapView: any;
 
   constructor() {}
 
   public ngOnInit(): void {
-    this.loadEsriModules().then(([EsriMap, EsriMapView]) => this.renderEsriMap(EsriMap, EsriMapView));
+    this.loadEsriModules().then(
+      ([EsriWebMap, EsriMapView, ArcGisPortal]) => {
+        this.arcgisPortal = this.loadArcgisPortal(ArcGisPortal);
+
+        this.arcgisPortal.load().then(() => {this.renderEsriMap(EsriWebMap, EsriMapView)});
+      }
+    );
   }
 
   public loadEsriModules(): Promise<any> {
     return loadModules([
       'esri/WebMap',
       'esri/views/MapView',
+      'esri/portal/Portal',
     ]);
   }
 
+  public initLayersFormGroup(): void {
+    this.layersFormGroup = new FormGroup({
+      populacao: new FormControl(true),
+      idhm: new FormControl(true),
+      pib: new FormControl(true),
+    });
+  }
+
+  public loadArcgisPortal(
+    ArcGisPortalClass: any,
+  ): any {
+    const arcgisPortal = new ArcGisPortalClass();
+    arcgisPortal.authMode = "immediate";
+
+    return arcgisPortal;
+  }
+
   public renderEsriMap(MapClass: any, MapViewClass: any): void {
-    const map = new MapClass({
-      // basemap: 'streets',
+     this.portalMap = new MapClass({
       portalItem: {
         id: '4cecdda8e94a4e48bbc2e75bedbee3ef',
       }
     });
 
-    const mapView = new MapViewClass({
+    this.portalMap.when(() => {
+      this.registerLayers();
+      this.initLayersFormGroup();
+      this.registerCheckboxListeners();
+      this.registerViewClickListeners();
+    });
+
+    this.mapView = new MapViewClass({
       container: this.mapViewElement.nativeElement,
-      map: map,
+      map: this.portalMap,
+      zoom: 6,
+    });
+  }
+
+  public registerLayers(): void {
+    this.populacaoLayer = this.portalMap.layers.items[0];
+    this.pibLayer = this.portalMap.layers.items[1];
+    this.idhmLayer = this.portalMap.layers.items[2];
+  }
+
+  public registerCheckboxListeners(): void {
+    this.layersFormGroup.get('populacao').valueChanges.subscribe(
+      ( layerIsVisible ) => {
+        this.populacaoLayer.visible = layerIsVisible;
+      }
+    );
+
+    this.layersFormGroup.get('idhm').valueChanges.subscribe(
+      ( layerIsVisible ) => {
+        this.idhmLayer.visible = layerIsVisible;
+      }
+    );
+
+    this.layersFormGroup.get('pib').valueChanges.subscribe(
+      ( layerIsVisible ) => {
+        this.pibLayer.visible = layerIsVisible;
+      }
+    );
+  }
+
+  public registerViewClickListeners(): void {
+    this.mapView.popup.autoOpenEnabled = false;
+
+    this.mapView.on('click', (event) => {
+      this.clickEventListener(event);
+    });
+  }
+
+  public clickEventListener(event: any): void {
+    this.mapView.popup.open({
+      title: 'Hello World',
+      content: 'Cidade fulaninha',
+      location: event.mapPoint
     });
   }
 }
